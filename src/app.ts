557cf06f996d3856e268a1f90fdb48e75717a0b8
sfh7698 <sharjeel.feroze@gmail.com>
2023-12-07 16:46:21 -0600

    Finish basic server setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express, { Express } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
 
 export default app;

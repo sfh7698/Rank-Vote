db7a86554758b51f4203d9f11ce5ca67fab6907f
sfh7698 <sharjeel.feroze@gmail.com>
2024-02-19 18:40:53 -0600

    add opacity to loader

--- client/src/pages/Loader.tsx
+++ client/src/pages/Loader.tsx
@@ -2,7 +2,7 @@ import { Page, ProgressCircular } from "react-onsenui";
 
 export default function Loader() {
     return (
-        <Page className="opacity-25">
+        <Page className="opacity-75">
             <div className="flex flex-col items-center justify-center h-full">
                 <ProgressCircular indeterminate />
             </div>

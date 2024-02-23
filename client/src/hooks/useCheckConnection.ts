a6b273bc67cf7fc539187578c635d01dc236e6e8
sfh7698 <sharjeel.feroze@gmail.com>
2024-02-23 15:18:55 -0600

    Add loading screen to waiting room

--- client/src/hooks/useCheckConnection.ts
+++ client/src/hooks/useCheckConnection.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { homeRoute } from "../utils";
 import { Navigator } from "react-onsenui";
 import { useAppSelector } from "../hooks/useAppSelector";
@@ -6,12 +6,17 @@ import { selectError } from "../app/slices/errorSlice";
 
 
 export function useCheckConnection(navigator: Navigator | undefined) {
+    const [isConnected, setIsConnected] = useState(true);
+
     const error = useAppSelector(selectError);
 
     useEffect(() => {
         if (error === "Failed to connect to poll") {
+            setIsConnected(false);
             navigator?.resetPage(homeRoute);
         }
 
     }, [error]);
+
+    return isConnected;
 }
--- client/src/pages/WaitingRoom.tsx
+++ client/src/pages/WaitingRoom.tsx
@@ -2,16 +2,18 @@ import { Page } from "react-onsenui";
 import { useStreamPollUpdatesQuery } from "../app/slices/socketSlice";
 import { Route } from "../utils";
 import { useCheckConnection } from "../hooks/useCheckConnection";
+import { Loader } from ".";
 
 export default function WaitingRoom({navigator}: Route["props"]) {
     const {data} = useStreamPollUpdatesQuery();
 
     console.log(data);
 
-    useCheckConnection(navigator);
+    const isLoading = useCheckConnection(navigator);
 
     return (
         <Page>
+            {(isLoading && !data) && <Loader />}
             <div>Waiting Room</div>
         </Page>
     )

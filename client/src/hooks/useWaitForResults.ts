ed639466013fc70935461d31b9e99532417b4029
sfh7698 <sharjeel.feroze@gmail.com>
2024-04-28 18:36:01 -0600

    User notified of poll end on voting page

---
+++ client/src/hooks/useWaitForResults.ts
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useAppSelector, useAppDispatch } from ".";
+import { selectPayloadFromToken } from "../app/slices/authSlice";
+import { selectPoll } from "../app/slices/pollSlice";
+import { setError } from "../app/slices/errorSlice";
+import { isJwtPayload } from "../utils";
+
+export function useWaitForResults() {
+    const poll = useAppSelector(selectPoll);
+    const usersHaveRanked = Object.keys(poll?.rankings || {});
+    const payload = useAppSelector(selectPayloadFromToken);
+
+    const dispatch = useAppDispatch();
+    
+    useEffect(() => {
+        if(poll?.results && Array.from(poll?.results).length > 0 && isJwtPayload(payload) && !usersHaveRanked.includes(payload.id)) {
+            dispatch(setError("Admin has ended voting"));
+            const timeout = setTimeout(() => {
+                location.reload()
+            }, 3000);
+    
+            return () => clearTimeout(timeout);
+        }
+    });
+}
